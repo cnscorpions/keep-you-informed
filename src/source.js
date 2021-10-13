70833295753348768fcd5dc496e55aab977a2aac
Chan Gavin <justin.coding@gmail.com>
2021-10-13 14:17:16 +0800

    知乎热榜抓取30条

--- src/source.js
+++ src/source.js
@@ -24,7 +24,9 @@ const listOfNewsSource = {
             'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1' // iphoneX 
         },
         rule: null,
-        fn: parseZhihu
+        fn: parseZhihu,
+        start: 0,
+        end: 29
     },
     bilibili: {
         text: "B站热搜",
